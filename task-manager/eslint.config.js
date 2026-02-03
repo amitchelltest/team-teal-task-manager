aceada76fd3eeb884f73b41296209d9cc889a1fc
KaylaCrush <kayla.e.rieck@gmail.com>
2026-02-02 17:38:15 -0800

    fixed linter going nuts about wrangler stuff

--- task-manager/eslint.config.js
+++ task-manager/eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', '.wrangler']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
@@ -24,6 +24,19 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+    },
+  },
+  {
+    files: [
+      'tests/components/**/*.{js,jsx,ts,tsx}',
+      'tests/integration/**/*.{js,jsx,ts,tsx}',
+    ],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
+      },
     },
   },
   {
