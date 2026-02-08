8b92638b64792dec42c97ba3a7b6d6075d5a1bba
Jay A <Jenna.Arends@Seattlecolleges.edu>
2026-02-07 20:54:21 -0800

    Moved to the pages directory starting w/ useEffect

---
+++ task-manager/src/pages/Profile.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+
+//Will need to be replaced with the actuall logged in user
+const CURRENT_USER_ID = 1;
+
+export default function Profile() {
+    //starting as small as possible
+    return (
+        <div>
+            <h2>TEST</h2>
+        </div>
+    );
+}
