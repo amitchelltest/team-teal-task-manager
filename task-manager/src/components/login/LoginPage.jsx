7b33635e010b4cb969f1b956595b04bb7d01b3cf
mizotii <mztiiiii0@gmail.com>
2026-02-11 21:36:45 -0800

    hardcode public client values for now so tests pass

--- task-manager/src/components/login/LoginPage.jsx
+++ task-manager/src/components/login/LoginPage.jsx
@@ -4,12 +4,12 @@ import { GoogleSignInButton } from './GoogleSignInButton';
 export default function LoginPage() {
   const authURLParams = new URLSearchParams({
             // todo: remove fallbacks once implemented
-            client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
-            redirect_uri: import.meta.env.VITE_GOOGLE_REDIRECT_URI,
+            client_id: "459719729310-mf1vkob8e7ljhcn44obrujolbsfpil27.apps.googleusercontent.com",
+            redirect_uri: "https://joel-logon-component-tt2s-39.team-teal-task-manager.pages.dev/api/auth/callback",
             response_type: 'code',
             scope: 'openid email profile'
         })
-  const authURL = new URL(import.meta.env.VITE_GOOGLE_AUTH_URL)
+  const authURL = new URL("https://accounts.google.com/o/oauth2/v2/auth")
   authURL.search = authURLParams.toString()
 
   return (
