8360ca5bba75be74f9745dbae0a180c0b2057617
Sandra <gran.sl@gmail.com>
2026-02-13 16:47:41 -0800

    Create Role enum

---
+++ task-manager/functions/api/constants/roles.js
@@ -0,0 +1,15 @@
+// This is making an ENUM in javascript.
+// Object.freeze makes an object immutable.
+export const USER_ROLES = Object.freeze({
+    ADMIN: "admin",
+    DEVELOPER: "developer",
+    CLINICIANS: "clinician",
+});
+
+// An helper array to check for user values
+export const USER_ROLE_VALUES = Object.freeze(Object.values(USER_ROLES));
+
+// Checks if a role is a valid role. 
+export function isValidUserRole(value) {
+    return USER_ROLE_VALUES.includes(value);
+}
