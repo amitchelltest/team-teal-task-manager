6891379a1d53d40884880e01fb75f57958e41100
KaylaCrush <kayla.e.rieck@gmail.com>
2026-03-03 10:48:25 -0800

    moved user-providers out of auth path so that it cannot be accessed withoiut authorization

--- task-manager/functions/api/user-providers.js
+++ task-manager/functions/api/user-providers.js
@@ -1,9 +1,9 @@
-import { makeCrudHandlers } from "../helpers.js";
+import { makeCrudHandlers } from "./helpers.js";
 
 const columnHandlers = makeCrudHandlers({
   table: "User_Providers",
   primaryKey: "id",
-  allowedColumns: ["user_id", "provider", "provider_user_id", "refresh_token", "token_expires_at", "created_at", "updated_at"],
+  allowedColumns: ["user_id", "provider", "provider_user_id", "token_expires_at", "created_at", "updated_at"],
   dbEnvVar: "cf_db",
   orderBy: "user_id",
 });
