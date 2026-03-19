545c6546ca54604cfc22c923fe299611bf9a9107
mizotii <mztiiiii0@gmail.com>
2026-03-18 17:52:14 -0700

    match to do regardless of capitalization

--- task-manager/tests/cypress/integration/creating-a-sprint.cy.js
+++ task-manager/tests/cypress/integration/creating-a-sprint.cy.js
@@ -6,7 +6,7 @@ describe('Sprint Creation Workflow', () => {
   });
 
   it('creates task in backlog and displays it correctly', () => {
-    const taskTitle = `my cool task ${Date.now()}`;
+    const taskTitle = `my cool backlog task ${Date.now()}`;
 
     // login -> dashboard -> select scrum project -> go to backlog -> create new task
     cy.goToBoard();
@@ -16,10 +16,31 @@ describe('Sprint Creation Workflow', () => {
     cy.get('#sprint-selection').should('be.visible');
     cy.contains('button', 'New Task').click();
     cy.get('input[name="title"]').type(taskTitle);
-    cy.get('textarea[name="description"]').type('my cool task description');
+    cy.get('textarea[name="description"]').type('my cool backlog task description');
     cy.contains('button', 'Create').click();
 
-    // assertion: task displays properly
+    // assertion: task displays properly in backlog
     cy.contains(taskTitle, { timeout: 10000 }).should('be.visible');
   });
-})
+
+  it('assigns task to sprint and displays it in sprint section', () => {
+    const taskTitle = `my cool backlog task ${Date.now()}`;
+
+    // login -> dashboard -> select scrum project -> go to backlog -> create new task -> assign to sprint
+    // -> select sprint
+    cy.goToBoard();
+    cy.selectProject('Scrum Sample Project');
+    cy.contains('button', 'Backlog').click();
+    cy.contains('Sprints').should('be.visible');
+    cy.get('#sprint-selection').should('be.visible');
+    cy.contains('button', 'New Task').click();
+    cy.get('input[name="title"]').type(taskTitle);
+    cy.get('textarea[name="description"]').type('my cool backlog task description');
+    cy.get('input[name="sprint_id"]').clear().type('2');
+    cy.contains('button', 'Create').click();
+    cy.get('#sprint-selection').select('2');
+
+    // assertion: task appears in sprint 1
+    cy.contains(taskTitle, { timeout: 10000 }).should('be.visible');
+  });
+});
